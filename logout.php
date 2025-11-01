1b2b985468080fdb5080b3fb057e527d3cbc8757
dosjanurazbaev-1 <dosjanurazbaev@gmail.com>
2025-11-01 08:18:12 +0500

    Implement CORS support in logout.php
    Added CORS headers and preflight handling for OPTIONS requests.

--- logout.php
+++ logout.php
@@ -1,6 +1,21 @@
 <?php
+// Разрешаем запросы с Tilda
+header("Access-Control-Allow-Origin: https://jailasu.tilda.ws");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
+// Обрабатываем preflight (OPTIONS)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+header('Content-Type: application/json; charset=utf-8');
+
 session_start();
 session_destroy();
 header("Location: login.php");
 exit;
 ?>
+
