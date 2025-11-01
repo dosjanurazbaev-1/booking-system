5eacfc8f14ebea7f6e312321c6f2bcc495fee3fe
dosjanurazbaev-1 <dosjanurazbaev@gmail.com>
2025-11-01 08:18:29 +0500

    Implement CORS support in register.php
    Added CORS headers to allow requests from Tilda.

--- register.php
+++ register.php
@@ -1,5 +1,18 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+// Разрешаем запросы с Tilda
+header("Access-Control-Allow-Origin: https://jailasu.tilda.ws");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
+// Обрабатываем preflight (OPTIONS)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+header('Content-Type: application/json; charset=utf-8');
 
 // Пусть файлы хранятся в каталоге /var/www/html/data
 $usersFile = __DIR__ . '/data/users.csv';
