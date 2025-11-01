7b83cea506ef6a9db5dcd9623e7e94777e2428b0
dosjanurazbaev-1 <dosjanurazbaev@gmail.com>
2025-11-01 08:17:27 +0500

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,4 +1,17 @@
 <?php
+// Разрешаем запросы с Tilda
+header("Access-Control-Allow-Origin: https://jailasu.tilda.ws");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
+// Обрабатываем preflight (OPTIONS)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+header('Content-Type: application/json; charset=utf-8');
 session_start();
 if (!isset($_SESSION['user'])) {
     header("Location: login.php");
@@ -18,3 +31,4 @@ if (file_exists("bookings.csv")) {
     echo "<p>Нет бронирований.</p>";
 }
 ?>
+
